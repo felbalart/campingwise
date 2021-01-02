b433c8c778962766c68e97263386cc44bc9a4265
Felipe Balart <felbalart@gmail.com>
2021-01-02 13:36:27 -0300

    feat(mailer): adds order email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def translate_date(date)
+    return unless date
+    day = translate_day date.strftime('%A')
+    rest = date.strftime('%d/%m/%Y')
+    "#{day}, #{rest}"
+  end
+
+  def translate_day(english_day)
+    case english_day.downcase
+    when 'monday'
+      'lunes'
+    when 'tuesday'
+      'martes'
+    when 'wednesday'
+      'miércoles'
+    when 'thursday'
+      'jueves'
+    when 'friday'
+      'viernes'
+    when 'saturday'
+      'sábado'
+    when 'sunday'
+      'domingo'
+    end
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,21 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  include ApplicationHelper
+  helper ApplicationHelper
+  
+  DEFAULT_FROM = ENV['DEFAULT_FROM_EMAIL']
+  default from: DEFAULT_FROM
   layout 'mailer'
 
   def test_email
     @user = 'soy user'
     mail(to: 'holabalart@gmail.com', subject: 'prueba email')
+  end
+
+
+  def order_email
+    @ord = params[:order]
+    to = [@ord.guest.email, DEFAULT_FROM].join(', ')
+    subject = "Orden de Reserva Nº #{@ord.id}"
+    mail(from: DEFAULT_FROM, to: to, subject: subject)
   end
 end
